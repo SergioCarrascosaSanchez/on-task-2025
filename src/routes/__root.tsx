5f40061ad593ac8e6ccf72894e40c2716bdae54d
Sergio Carrascosa <sergio.carrascosa@the-cliff.com>
2025-06-16 20:04:05 +0200

    chore: config alias

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { AppProvider } from "../app/AppProvider";
+import { AppProvider } from "@/app/AppProvider";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
