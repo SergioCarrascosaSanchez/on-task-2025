581e6cb11724bdebd779bdc418bee227c18585de
Sergio Carrascosa <sergio.carrascosa@the-cliff.com>
2025-06-22 20:25:27 +0200

    chore: adds React Hot Toast

--- src/app/AppProvider.tsx
+++ src/app/AppProvider.tsx
@@ -5,12 +5,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "./store";
 import { queryClient } from "./queryClient";
 import "@/shared/config/i18n";
+import { Toaster } from "react-hot-toast";
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
     <ReduxProvider store={store}>
       <QueryClientProvider client={queryClient}>
         {children}
+        <Toaster position="bottom-center" />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ReduxProvider>
