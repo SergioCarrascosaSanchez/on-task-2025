d584fdc32875bbf1a704a920dcb5adf7444a10a9
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2025-08-11 17:55:54 +0200

    chore: removes redux

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,19 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { AppProvider } from "@/app/AppProvider";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "@/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: () => (
-    <AppProvider>
+    <QueryClientProvider client={queryClient}>
       <Outlet />
       <TanStackRouterDevtools />
-    </AppProvider>
+      <Toaster position="bottom-center" />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   ),
 });
