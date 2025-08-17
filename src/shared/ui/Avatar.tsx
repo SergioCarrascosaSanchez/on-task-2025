b181c4065543e60b5c40d36b03a56381cdd3f6c2
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2025-08-17 18:49:39 +0200

    feat: adds avatar

---
+++ src/shared/ui/Avatar.tsx
@@ -0,0 +1,112 @@
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { cn } from "@/shared/lib/utils";
+import { Typography } from "./Typography";
+
+function AvatarBase({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
+  return (
+    <AvatarPrimitive.Root
+      data-slot="avatar"
+      className={cn(
+        "relative flex size-10 shrink-0 overflow-hidden rounded-full",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function AvatarImage({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+  return (
+    <AvatarPrimitive.Image
+      data-slot="avatar-image"
+      className={cn("aspect-square size-full", className)}
+      {...props}
+    />
+  );
+}
+
+function AvatarFallback({
+  className,
+  letters,
+  bgClass,
+  textClass,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Fallback> & {
+  bgClass: string;
+  textClass: string;
+  letters: string;
+}) {
+  return (
+    <AvatarPrimitive.Fallback
+      data-slot="avatar-fallback"
+      className={cn(
+        "flex size-full items-center justify-center rounded-full",
+        bgClass,
+        className
+      )}
+      {...props}
+    >
+      <Typography variant="small" className={cn("font-bold", textClass)}>
+        {letters}
+      </Typography>
+    </AvatarPrimitive.Fallback>
+  );
+}
+
+function Avatar({
+  className,
+  image,
+  name,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root> & {
+  image?: string;
+  name: string;
+}) {
+  const { bg, text } = getFallbackColorClasses(name);
+  return (
+    <AvatarBase className={className} {...props}>
+      {image ? (
+        <AvatarImage src={image} />
+      ) : (
+        <AvatarFallback
+          letters={getFallbackLetters(name)}
+          bgClass={bg}
+          textClass={text}
+        />
+      )}
+    </AvatarBase>
+  );
+}
+
+function getFallbackColorClasses(name: string): { bg: string; text: string } {
+  const palette: Array<{ bg: string; text: string }> = [
+    { bg: "bg-red-200/50", text: "text-red-800" },
+    { bg: "bg-orange-200/50", text: "text-orange-800" },
+    { bg: "bg-amber-200/50", text: "text-amber-800" },
+    { bg: "bg-green-200/50", text: "text-green-800" },
+    { bg: "bg-teal-200/50", text: "text-teal-800" },
+    { bg: "bg-blue-200/50", text: "text-blue-800" },
+    { bg: "bg-indigo-200/50", text: "text-indigo-800" },
+    { bg: "bg-purple-200/50", text: "text-purple-800" },
+  ];
+  if (!name) return palette[0];
+  const code = name.charCodeAt(0) || 0;
+  const i = code % palette.length;
+  return palette[i];
+}
+
+function getFallbackLetters(name: string) {
+  const nameArray = name.trim().split(/\s+/).filter(Boolean);
+  if (nameArray.length === 0) return "";
+  if (nameArray.length === 1) return nameArray[0][0].toUpperCase();
+  return `${nameArray[0][0].toUpperCase()}${nameArray[1][0].toUpperCase()}`;
+}
+
+export { Avatar, AvatarImage, AvatarFallback };
--- src/shared/ui/Sidebar/SidebarAccountItem.tsx
+++ src/shared/ui/Sidebar/SidebarAccountItem.tsx
@@ -1,17 +1,24 @@
-import { UserIcon } from "lucide-react";
+import { cn } from "@/shared/lib/utils";
 import { Typography } from "../Typography";
+import { Avatar } from "../avatar";
 
 interface SidebarAccountItemProps {
   isExtended: boolean;
 }
 
 export function SidebarAccountItem({ isExtended }: SidebarAccountItemProps) {
+  const commonClasses = "mb-4";
+  const isExtendedClasses = "flex gap-3 items-center";
+  const isNotExtendedClasses = "grid place-content-center";
   return (
-    <div className="flex gap-3 mb-4 items-center">
+    <div
+      className={cn(
+        commonClasses,
+        isExtended ? isExtendedClasses : isNotExtendedClasses
+      )}
+    >
       <div className="rounded-full border-muted-900 p-0.5 grid place-content-center border-solid border-1">
-        <div className="rounded-full bg-blue-100/80 p-3">
-          <UserIcon className="size-6" />
-        </div>
+        <Avatar name={"Test User"} />
       </div>
       {isExtended && (
         <div>
